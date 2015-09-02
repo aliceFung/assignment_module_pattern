411ccaf3990def66d0dda3f321267e19c3d7fd00
mikebutts <mgbutts@gmail.com>
2015-09-02 12:23:33 -0400

    dependency

--- script2.js
+++ script2.js
@@ -1,14 +1,14 @@
 //practice with revealing modules, returns obj with public methods
 var mod = mod || {}
 
-  mod.RevealingModule = (function(){
+mod.RevealingModule2 = (function(){
 
   //private variables
-  var _superSecretLottoNumbers = [1,3,5,7,9,11,13];
+  var _superSecretLottoNumbers = [2,4,6,8,10,12,15];
   var stub = {};
 
   var totalNumAccepted = _superSecretLottoNumbers.length;
-  stub.checkLottoNumMatch = function(picks){
+  stub.checkLottoNumMatch2 = function(picks){
     var matches = 0;
     for (var i=0; i < _superSecretLottoNumbers.length; i++){
       if (picks[i] === _superSecretLottoNumbers[i]){
@@ -25,4 +25,3 @@ var mod = mod || {}
   return stub;
 
 })();
-
